4ba4af5a2e49e698294220102a0c6c06f2f29858
luc1dLife <luc1d_life@protonmail.com>
2020-08-26 01:46:35 -0600

    Layout Progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var json = '{"q1": "Commonly used data types do NOT include:", "q1a1": "Strings", "q1a2": "Booleans", "q1a3": "Alerts", "q1a4": "Numbers"}';
+var json = '{"q1": "Commonly used data types do NOT include:", "q1a1": "Strings", "q1a2": "Booleans", "q1a3": "Alerts", "q1a4": "Numbers", "q1a": "q1a2"}';
+
 var obj = JSON.parse(json);
 document.write(obj.q1 + "</br>");
 document.write(obj.q1a1 + "</br>");
