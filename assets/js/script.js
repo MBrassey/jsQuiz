b98230c65e642d820fec67ed51b6b7806fc81afd
luc1dLife <luc1d_life@protonmail.com>
2020-08-25 22:50:09 -0600

    initial script

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var json = '{"q1": "Commonly used data types do NOT include:", "q1a1": "Strings", "q1a2": "Booleans", "q1a3": "Alerts", "q1a4": "Numbers"}';
+var obj = JSON.parse(json);
+document.write(obj.q1 + "</br>");
+document.write(obj.q1a1 + "</br>");
+document.write(obj.q1a2 + "</br>");
+document.write(obj.q1a3 + "</br>");
+document.write(obj.q1a4 + "</br>");
