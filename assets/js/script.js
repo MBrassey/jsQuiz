f3074d8b70e2d2f2f25050c3f88d012c8c18c968
luc1dLife <luc1d_life@protonmail.com>
2020-08-27 03:34:22 -0600

    Working on actual loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,62 @@
-var json =
-    '{"q1": "Commonly used data types do NOT include:", "q1a1": "Strings", "q1a2": "Booleans", "q1a3": "Alerts", "q1a4": "Numbers", "q1a": "Alerts", "q2": "The condition of an if/ else statement is enclosed with __________:", "q2a1": "Quotes", "q2a2": "Curly Brackets", "q2a3": "Parentheses", "q2a4": "Square Brackets", "q2a": "Parentheses", "q3": "Arrays is Javascript can be used to store:", "q3a1": "Numbers & Strings", "q3a2": "Other Arrays", "q3a3": "Booleans", "q3a4": "All of the above", "q3a": "All of the above","q4": "String values must be enclosed within _______ when being assigned to variables:", "q4a1": "Commas", "q4a2": "Curly Brackets", "q4a3": "Quotes", "q4a4": "Parentheses", "q4a": "Quotes","q5": "A very useful tool used during development & debugging for printing content to the debugger is:", "q5a1": "Javascript", "q5a2": "Terminal/Bash", "q5a3": "For Loops", "q5a4": "Console.log", "q5a": "Console.log"}';
+// Initial Click Area
 var answerContentEl = document.querySelector("#answer");
+var score = 0;
 
-var obj = JSON.parse(json);
-document.write(obj.q1 + "</br>");
-document.write(obj.q1a1 + "</br>");
-document.write(obj.q1a2 + "</br>");
-document.write(obj.q1a3 + "</br>");
-document.write(obj.q1a4 + "</br>");
+
+// Questions Array
+var questions = [
+    { q: "Commonly used data types do NOT include:", ca: "Alerts" },
+    { q: "The condition of an if/ else statement is enclosed with __________:", ca: "Parentheses" },
+    { q: "Arrays is Javascript can be used to store:", ca: "All of the above" },
+    { q: "String values must be enclosed within _______ when being assigned to variables:", ca: "Quotes" },
+    { q: "A very useful tool used during development & debugging for printing content to the debugger is:", ca: "Console.log" },
+];
+
+// Answers Array
+var answers = [
+    { 1: "Strings", 2: "Booleans", 3: "Alerts", 4: "Numbers" },
+    { 1: "Quotes", 2: "Curly Brackets", 3: "Parentheses", 4: "Square Brackets" },
+    { 1: "Numbers & Strings", 2: "Other Arrays", 3: "Booleans", 4: "All of the above" },
+    { 1: "Commas", 2: "Curly Brackets", 3: "Quotes", 4: "Parentheses" },
+    { 1: "Javascript", 2: "Terminal/Bash", 3: "For Loops", 4: "Console.log" },
+];
 
 var startQuiz = function () {
-    console.log("Starting Quiz");
-    var Question = document.getElementById("questionLine");
 
-    var Answer = document.getElementById("answer");
-    var answerEl = document.createElement("div");
+      // Loop over every question object
+      for (var i = 0; i < questions.length; i++) {
 
-    // Load Question
-    Question.innerHTML = "";
-    Question.innerHTML = "<h2>" + obj.q1 + "</h2>"
 
-    // Load Answers
-    Answer.innerHTML = "";
-    answerEl.className = "answerList";
-    answerEl.innerHTML = "<h3 class='answerText'>" + obj.q1a1 + "</h3><h3 class='answerText'>" + obj.q1a2 + "</h3><h3 class='answerText'>" + obj.q1a3 + "</h3><h3 class='answerText'>" + obj.q1a4 + "</h3>";
-    document.getElementById('answer').appendChild(answerEl);
-}
+        console.log("Question: " + i);
+        var Question = document.getElementById("questionLine");
+        var Answer = document.getElementById("answer");
+        var answerEl = document.createElement("div");
+        var qnum = 1;
+    
+        // Load Questions
+        Question.innerHTML = "";
+        Question.innerHTML = "<h2>" + questions[i].q + "</h2>";
+    
+        // Load Answers
+        Answer.innerHTML = "";
+        answerEl.className = "answerList";
+        answerEl.innerHTML = "<h3 class='answerText'>" + questions[i].ca + "</h3>";
+        document.getElementById("answer").appendChild(answerEl);
+
+
+        // Compare answers
+        if (
+          (answer === true && questions[i].a === 't') ||
+          (answer === false && questions[i].a === 'f')
+        ) {
+          // Increase score
+          score++;
+          alert('Correct!');
+        } else {
+          alert('Wrong!');
+        }
+      }
+};
 
 var clickHandler = function (event) {
     // event.preventDefault();
@@ -37,7 +68,6 @@ var clickHandler = function (event) {
     if (targetEl.matches("#begin")) {
         console.log("Start was clicked.");
         startQuiz();
-
     } else if (targetEl.matches("#answer")) {
         console.log("Answer was clicked.");
     }
